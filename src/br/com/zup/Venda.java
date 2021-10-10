c08426d2e9488d0eafa331121da6e5d715959b09
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-10-10 17:39:12 -0300

    Criando Construtor junto dos Getters e Setters da classe Venda.

--- src/br/com/zup/Venda.java
+++ src/br/com/zup/Venda.java
@@ -5,4 +5,43 @@ public class Venda {
     VendedorResponsável vendResponsavel;
     double valor;
     String dataRegistro;
+
+    public Venda(Cliente cliente, VendedorResponsável vendResponsavel, double valor, String dataRegistro) {
+        this.cliente = cliente;
+        this.vendResponsavel = vendResponsavel;
+        this.valor = valor;
+        this.dataRegistro = dataRegistro;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public VendedorResponsável getVendResponsavel() {
+        return vendResponsavel;
+    }
+
+    public void setVendResponsavel(VendedorResponsável vendResponsavel) {
+        this.vendResponsavel = vendResponsavel;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDataRegistro() {
+        return dataRegistro;
+    }
+
+    public void setDataRegistro(String dataRegistro) {
+        this.dataRegistro = dataRegistro;
+    }
 }
