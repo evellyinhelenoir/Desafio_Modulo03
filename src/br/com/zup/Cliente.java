0cd92cae6625991438201fe2417c99187d7f8c5c
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-10-10 17:30:46 -0300

    Criando Construtor com Getters e Setters do Cliente

--- src/br/com/zup/Cliente.java
+++ src/br/com/zup/Cliente.java
@@ -3,4 +3,25 @@ package br.com.zup;
 public class Cliente {
     private String email;
     private String cpf;
+
+    public Cliente(String email, String cpf) {
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
