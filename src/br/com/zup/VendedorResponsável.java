2c883e02249b59c9070c9bc430aff89654d222fe
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-10-10 17:35:57 -0300

    Criando Construtor junto dos Getters e Setters do Vendedor Responsável.

--- src/br/com/zup/VendedorResponsável.java
+++ src/br/com/zup/VendedorResponsável.java
@@ -1,7 +1,37 @@
 package br.com.zup;
 
 public class VendedorResponsável {
+    private String nome;
     private String email;
     private String cpf;
 
+    public VendedorResponsável(String nome, String email, String cpf) {
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
