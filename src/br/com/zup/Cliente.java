9197a1ec38aecd7fae28c23329fcdef4d0f5bc02
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-10-10 17:34:36 -0300

    Adicionando nome como atributo do Cliente e incluindo ao Construtor, Getters e Setters e ToString.

--- src/br/com/zup/Cliente.java
+++ src/br/com/zup/Cliente.java
@@ -1,12 +1,14 @@
 package br.com.zup;
 
 public class Cliente {
+    private String nome;
     private String email;
     private String cpf;
 
-    public Cliente(String email, String cpf) {
+    public Cliente(String email, String cpf, String nome) {
         this.email = email;
         this.cpf = cpf;
+        this.nome = nome;
     }
 
     public String getEmail() {
@@ -25,9 +27,18 @@ public class Cliente {
         this.cpf = cpf;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     @Override
     public String toString() {
         StringBuilder retorno = new StringBuilder();
+        retorno.append("\nNome do Consumidor: "+nome);
         retorno.append("\nEmail do Consumidor: "+email);
         retorno.append("\nCPF do Consumidor: "+cpf);
         return retorno.toString();
